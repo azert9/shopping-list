a09c4ef48b984c59dabaeff9db440ad69f2eba6a
azert9 <96430950+azert9@users.noreply.github.com>
2026-09-08 21:26:45 +0200

    chore: version 0.1.0-rc.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "fr.jloc.shoppinglist"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0-rc.1"
+        versionCode = 2
+        versionName = "0.1.0-rc.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
